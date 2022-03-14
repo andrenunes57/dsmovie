5eaf43e663d2a7350bbb18c12494ee414bb552a2
andre.nunes <andre.nunes@farfetch.com>
2022-03-14 17:08:58 -0300

    erase /index from Listing

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,5 @@
 import MovieCard from "components/MovieCard";
-import Pagination from "components/Pagination/index";
+import Pagination from "components/Pagination";
 
 function Listing() {
   return (
